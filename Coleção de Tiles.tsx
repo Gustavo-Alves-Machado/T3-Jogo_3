8a9647f377f4235fc546b95f165b13fc077ab456
Gustavo-Alves-Machado <gustavo.alvmac@usp.br>
2021-12-21 00:50:13 -0300

    Trabalho com mapa
    -Adicionei novos assets pro mapa
    -Alterei no arquivo pro mapa

---
+++ Coleção de Tiles.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Monstro_Direita_0" tilewidth="101" tileheight="151" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="101" height="151" source="Imagens para as Tiles/Armário 100 x 150.png"/>
+ </tile>
+ <tile id="2">
+  <image width="101" height="51" source="Imagens para as Tiles/Mesa 100 x 50.png"/>
+ </tile>
+ <tile id="3">
+  <image width="101" height="26" source="Imagens para as Tiles/Mesinha 100 x 25.png"/>
+ </tile>
+</tileset>
